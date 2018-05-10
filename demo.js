a0d377ec461b4733d98ef423fdf87fc182599f4d
Briny131 <37700191+Briny131@users.noreply.github.com>
2018-05-10 10:25:14 +0800

    ()=>

--- demo.js
+++ demo.js
@@ -1,17 +1,17 @@
-window.onload=function(){
-	$('.choose').eq(0).hover(function(){
+window.onload=()=>{
+	$('.choose').eq(0).hover(function(){ //这里如果使用箭头函数，内部调用this指向的是window对象而不是调用函数对象
 		$(this).toggleClass('green');
-	}).click(function(){
+	}).click(()=>{
 		window.location.href='demo/snack.html?120'
 	});
 	$('.choose').eq(1).hover(function(){
 		$(this).toggleClass('yellow');
-	}).click(function(){
+	}).click(()=>{
 		window.location.href='demo/snack.html?60'
 	});
 	$('.choose').eq(2).hover(function(){
 		$(this).toggleClass('red');
-	}).click(function(){
+	}).click(()=>{
 		window.location.href='demo/snack.html?30'
 	});
 }
